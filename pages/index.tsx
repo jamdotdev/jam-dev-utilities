be0b06c558c35fade48deb922926e4708caa9b71
peckz <pekiseven@gmail.com>
2024-07-24 19:59:12 +0200

    fix: wrap cards with <Link/>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import HomeCard from "../components/HomeCard";
 import { ThemeToggle } from "../components/ThemeToggle";
 
@@ -33,6 +34,7 @@ export default function Home() {
             key={card.title}
             title={card.title}
             description={card.description}
+            link={card.link}
           />
         ))}
       </div>
@@ -45,40 +47,48 @@ const cardData = [
     title: "Timestamp to date",
     description:
       "Easily convert epoch time (also known as Unix time or Unix timestamp) to a human-readable month/day/year and time format.",
+    link: "",
   },
   {
     title: "HEX to RGB",
     description:
       "Quickly convert hexadecimal color codes to their corresponding RGB values for easy use in various programming contexts.",
+    link: "",
   },
   {
     title: "CSV to JSON",
     description:
       "Seamlessly transform CSV data into JSON format, enabling easy data interchange and parsing across different systems and applications.",
+    link: "",
   },
   {
     title: "Base64 encoder/decoder",
     description:
       "Effortlessly encode and decode text or files to and from Base64 format, facilitating data transmission and storage in a web-safe text format.",
+    link: "utilities/base-64-encoder",
   },
   {
     title: "JSON formatter",
     description:
       "Beautify and structure raw JSON data with proper indentation and formatting, making it easier to read, edit, and validate JSON content.",
+    link: "",
   },
   {
     title: "YAML to JSON",
     description:
       "Effortlessly convert YAML data to JSON format, enabling smooth transitions between different data serialization formats in your projects.",
+    link: "",
   },
   {
     title: "Query params to JSON",
     description:
       "Convert URL query parameters into a structured JSON object, simplifying the process of parsing and manipulating URL data in web applications.",
+    link: "",
   },
   {
     title: "URL encoder/decoder",
     description:
       "Encode and decode URLs to ensure proper handling of special characters, spaces, and non-ASCII symbols in web addresses and query strings.",
+    link: "",
   },
 ];
