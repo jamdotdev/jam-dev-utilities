ebd6c98d25cabe8071f8617f2c2a8364378fb200
peckz <pekiseven@gmail.com>
2024-07-24 19:53:48 +0200

    style: tweak button height, add hover effect to card component

--- components/ds/CardComponent.tsx
+++ components/ds/CardComponent.tsx
@@ -8,7 +8,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm flex flex-1",
+      "rounded-lg border bg-card text-card-foreground shadow-sm flex flex-1 hover:shadow-md transition",
       className
     )}
     {...props}
