c139536899a59f5d4ba904c75dd9449e0eb6e589
peckz <pekiseven@gmail.com>
2024-07-24 13:58:13 +0200

    chore: setup necessary theme provider for shadcn-ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
