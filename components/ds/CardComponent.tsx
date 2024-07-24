f729c33906b0b940300828e1cc466af0460834c4
peckz <pekiseven@gmail.com>
2024-07-24 13:15:50 +0200

    feat: add grid structure for home page
    - added ButtonComponent
    - added CardComponent

---
+++ components/ds/CardComponent.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { cn } from "@/lib/utils";
+
+const Card = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "rounded-lg border bg-card text-card-foreground shadow-sm flex flex-1",
+      className
+    )}
+    {...props}
+  />
+));
+Card.displayName = "Card";
+
+const CardHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className={cn("flex flex-col p-7", className)} {...props} />
+));
+CardHeader.displayName = "CardHeader";
+
+const CardTitle = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+  <h3
+    ref={ref}
+    className={cn(
+      "text-base font-semibold leading-none tracking-tight mb-3",
+      className
+    )}
+    {...props}
+  />
+));
+CardTitle.displayName = "CardTitle";
+
+const CardDescription = React.forwardRef<
+  HTMLParagraphElement,
+  React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+  <p
+    ref={ref}
+    className={cn("mb-5 flex-1 text-sm text-muted-foreground", className)}
+    {...props}
+  />
+));
+CardDescription.displayName = "CardDescription";
+
+const CardContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("p-6 items-baseline flex flex-col", className)}
+    {...props}
+  />
+));
+CardContent.displayName = "CardContent";
+
+const CardFooter = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex items-center p-6 flex-1 pt-0", className)}
+    {...props}
+  />
+));
+CardFooter.displayName = "CardFooter";
+
+export {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,60 @@
+import HomeCard from "../components/HomeCard";
+
 export default function Home() {
-  return <main>Jam.dev Utilities</main>;
+  return (
+    <main>
+      <div className="container grid grid-cols-3 gap-6">
+        {cardData.map((card) => (
+          <HomeCard
+            key={card.title}
+            title={card.title}
+            description={card.description}
+          />
+        ))}
+      </div>
+    </main>
+  );
 }
+
+const cardData = [
+  {
+    title: "Timestamp to date",
+    description:
+      "Easily convert epoch time (also known as Unix time or Unix timestamp) to a human-readable month/day/year and time format.",
+  },
+  {
+    title: "HEX to RGB",
+    description:
+      "Quickly convert hexadecimal color codes to their corresponding RGB values for easy use in various programming contexts.",
+  },
+  {
+    title: "CSV to JSON",
+    description:
+      "Seamlessly transform CSV data into JSON format, enabling easy data interchange and parsing across different systems and applications.",
+  },
+  {
+    title: "Base64 encoder/decoder",
+    description:
+      "Effortlessly encode and decode text or files to and from Base64 format, facilitating data transmission and storage in a web-safe text format.",
+  },
+  {
+    title: "JSON formatter",
+    description:
+      "Beautify and structure raw JSON data with proper indentation and formatting, making it easier to read, edit, and validate JSON content.",
+  },
+  {
+    title: "YAML to JSON",
+    description:
+      "Effortlessly convert YAML data to JSON format, enabling smooth transitions between different data serialization formats in your projects.",
+  },
+  {
+    title: "Query params to JSON",
+    description:
+      "Convert URL query parameters into a structured JSON object, simplifying the process of parsing and manipulating URL data in web applications.",
+  },
+  {
+    title: "URL encoder/decoder",
+    description:
+      "Encode and decode URLs to ensure proper handling of special characters, spaces, and non-ASCII symbols in web addresses and query strings.",
+  },
+];
