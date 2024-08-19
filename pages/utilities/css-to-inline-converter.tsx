dd5038da7f46d2b26f77454590b7b93efce1bbab
Eduardo Giacometti De Patta <eduardo.depatta@gmail.com>
2024-08-19 18:22:00 -0300

    feat(page): implement CSS to Inline Converter page Added the CSSToInlineConverter page, allowing users to input HTML and CSS, convert the CSS to inline styles, and display the result.

---
+++ pages/utilities/css-to-inline-converter.tsx
@@ -0,0 +1,124 @@
+import { useCallback, useState } from "react";
+import { Textarea } from "@/components/ds/TextareaComponent";
+import PageHeader from "@/components/PageHeader";
+import { Card } from "@/components/ds/CardComponent";
+import { Button } from "@/components/ds/ButtonComponent";
+import { Label } from "@/components/ds/LabelComponent";
+import Header from "@/components/Header";
+import { useCopyToClipboard } from "@/components/hooks/useCopyToClipboard";
+import CallToActionGrid from "@/components/CallToActionGrid";
+import Meta from "@/components/Meta";
+import { convertCSSToInline } from "../../components/utils/css-to-inline.utils";
+import { CMDK } from "../../components/CMDK";
+
+export default function CSSToInlineConverter() {
+  const [htmlInput, setHtmlInput] = useState("");
+  const [cssInput, setCssInput] = useState("");
+  const [output, setOutput] = useState("");
+  const { buttonText, handleCopy } = useCopyToClipboard();
+
+  function isValidHTML(html: string): boolean {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+
+    const hasTags =
+      doc.body.innerHTML.trim() !== "" && doc.body.children.length > 0;
+    const hasParsingErrors = doc.querySelectorAll("parsererror").length > 0;
+
+    return hasTags && !hasParsingErrors;
+  }
+
+  const handleHtmlChange = useCallback(
+    (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setHtmlInput(event.currentTarget.value);
+    },
+    []
+  );
+
+  const handleCssChange = useCallback(
+    (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setCssInput(event.currentTarget.value);
+    },
+    []
+  );
+
+  const handleConvert = useCallback(() => {
+    const trimmedHtml = htmlInput.trim();
+    const trimmedCss = cssInput.trim();
+
+    const conditions = [
+      {
+        condition: !trimmedHtml || !trimmedCss,
+        message: "Please provide both HTML and CSS inputs.",
+      },
+      {
+        condition: !isValidHTML(trimmedHtml),
+        message: "Invalid HTML input. Please check your HTML.",
+      },
+    ];
+
+    const errorMessage =
+      conditions.find((value) => value.condition)?.message || null;
+
+    try {
+      const inlinedHtml = convertCSSToInline(htmlInput, cssInput);
+      setOutput(errorMessage ?? inlinedHtml);
+    } catch (errorMessage: unknown) {
+      setOutput(errorMessage as string);
+    }
+  }, [htmlInput, cssInput]);
+
+  return (
+    <main>
+      <Meta
+        title="CSS to Inline Converter by Jam.dev | Free, Open Source & Ad-free"
+        description="Convert CSS styles to inline styles directly in your HTML with Jam's free CSS to inline converter. Just paste your HTML and CSS, and get the inlined HTML result."
+      />
+      <Header />
+      <CMDK />
+
+      <section className="container max-w-2xl mb-12">
+        <PageHeader
+          title="CSS to Inline Converter"
+          description="Convert your CSS to inline styles quickly and easily."
+        />
+      </section>
+
+      <section className="container max-w-2xl mb-6">
+        <Card className="flex flex-col p-6 hover:shadow-none shadow-none rounded-xl">
+          <div>
+            <Label>HTML</Label>
+            <Textarea
+              rows={6}
+              placeholder="Paste HTML here"
+              onChange={handleHtmlChange}
+              className="mb-6"
+              value={htmlInput}
+            />
+
+            <Label>CSS</Label>
+            <Textarea
+              rows={6}
+              placeholder="Paste CSS here"
+              onChange={handleCssChange}
+              className="mb-6"
+              value={cssInput}
+            />
+
+            <Button onClick={handleConvert} className="mb-4">
+              Convert to Inline
+            </Button>
+
+            <Label>Inlined HTML</Label>
+            <Textarea value={output} rows={6} readOnly className="mb-4" />
+            <Button variant="outline" onClick={() => handleCopy(output)}>
+              {buttonText}
+            </Button>
+          </div>
+        </Card>
+      </section>
+
+      <CallToActionGrid />
+    </main>
+  );
+}
