19e9b25f61b7ed566e6d4618854ea83b09a72807
Eduardo Giacometti De Patta <eduardo.depatta@gmail.com>
2024-08-20 20:38:51 -0300

    feat(tailwindcss): add grow-from-center animation to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -67,10 +67,24 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "grow-from-center": {
+          "0%": {
+            transform: "scale(0)",
+            opacity: "0",
+          },
+          "50%": {
+            transform: "scale(1.05)",
+            opacity: "1",
+          },
+          "100%": {
+            transform: "scale(1)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "grow-from-center": "grow-from-center 0.5s ease-out forwards",
       },
     },
   },
