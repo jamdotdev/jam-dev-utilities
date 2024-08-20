35a08e4c495c99ad3dece9e5d8487fc4e340aba8
Eduardo Giacometti De Patta <eduardo.depatta@gmail.com>
2024-08-20 17:30:33 -0300

    refactor(resize-image): use options object and mantainAspectRatio

--- components/utils/resize-image.utils.ts
+++ components/utils/resize-image.utils.ts
@@ -1,36 +1,51 @@
-export function resizeImage(
-  img: HTMLImageElement,
-  width?: number,
-  height?: number,
-  format: "png" | "jpeg" = "png",
-  quality: number = 1
-): Promise<string> {
+interface ResizeImageOptions {
+  img: HTMLImageElement;
+  width?: number;
+  height?: number;
+  format?: "png" | "jpeg";
+  quality?: number;
+  maintainAspectRatio?: boolean;
+}
+
+export function resizeImage({
+  img,
+  format,
+  height,
+  maintainAspectRatio,
+  quality,
+  width,
+}: ResizeImageOptions): Promise<string> {
   return new Promise((resolve) => {
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContext("2d");
 
-    const ratio = img.width / img.height;
-
-    const dimensionMapper = {
-      byWidth: () => {
-        canvas.width = width!;
-        canvas.height = width! / ratio;
-      },
-      byHeight: () => {
-        canvas.height = height!;
-        canvas.width = height! * ratio;
-      },
-      byDefault: () => {
-        canvas.width = img.width;
-        canvas.height = img.height;
-      },
-    };
-
-    const key = width ? "byWidth" : height ? "byHeight" : "byDefault";
-    dimensionMapper[key]();
-
     if (!ctx) {
       return;
+    }
+
+    if (maintainAspectRatio) {
+      const ratio = img.width / img.height;
+
+      const dimensionMapper = {
+        byWidth: () => {
+          canvas.width = width!;
+          canvas.height = width! / ratio;
+        },
+        byHeight: () => {
+          canvas.height = height!;
+          canvas.width = height! * ratio;
+        },
+        byDefault: () => {
+          canvas.width = img.width;
+          canvas.height = img.height;
+        },
+      };
+
+      const key = width ? "byWidth" : height ? "byHeight" : "byDefault";
+      dimensionMapper[key]();
+    } else {
+      canvas.width = width || img.width;
+      canvas.height = height || img.height;
     }
 
     ctx.imageSmoothingEnabled = true;
