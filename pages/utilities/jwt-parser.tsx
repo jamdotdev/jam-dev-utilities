0cf0c911d8303a470f88292f2adfe7ca708b63b9
Eduardo Giacometti De Patta <eduardo.depatta@gmail.com>
2024-08-23 11:41:23 -0300

    feat: create jwt-parser pages component

---
+++ pages/utilities/jwt-parser.tsx
@@ -0,0 +1,115 @@
+import { ChangeEvent, useCallback, useState } from "react";
+import { Textarea } from "@/components/ds/TextareaComponent";
+import PageHeader from "@/components/PageHeader";
+import { Card } from "@/components/ds/CardComponent";
+import { Button } from "@/components/ds/ButtonComponent";
+import { Label } from "@/components/ds/LabelComponent";
+import Header from "@/components/Header";
+import { CMDK } from "@/components/CMDK";
+import { useCopyToClipboard } from "@/components/hooks/useCopyToClipboard";
+import CallToActionGrid from "@/components/CallToActionGrid";
+import Meta from "@/components/Meta";
+import { decodeJWT } from "../../components/utils/jwt-parser.utils";
+
+export default function JWTParser() {
+  const [input, setInput] = useState("");
+  const [header, setHeader] = useState("");
+  const [payload, setPayload] = useState("");
+  const [signature, setSignature] = useState("");
+
+  const { buttonText: headerText, handleCopy: handleCopyHeader } =
+    useCopyToClipboard();
+  const { buttonText: payloadText, handleCopy: handleCopyPayload } =
+    useCopyToClipboard();
+  const { buttonText: signatureText, handleCopy: handleCopySignature } =
+    useCopyToClipboard();
+
+  const handleChange = useCallback(
+    (event: ChangeEvent<HTMLTextAreaElement>) => {
+      const value = event.currentTarget.value;
+      setInput(value);
+
+      try {
+        const { header, payload, signature } = decodeJWT(value.trim());
+        setHeader(JSON.stringify(header, null, 2));
+        setPayload(JSON.stringify(payload, null, 2));
+        setSignature(signature || "");
+      } catch (error) {
+        const errorMessage =
+          error instanceof Error ? error.message : "Invalid Input.";
+        setHeader(errorMessage);
+        setPayload(errorMessage);
+        setSignature(errorMessage);
+      }
+    },
+    []
+  );
+
+  return (
+    <main>
+      <Meta
+        title="JWT Parser by Jam.dev | Free, Open Source & Ad-free"
+        description="Decode JWT tokens quickly and easily with Jam's free online JWT Decoder. Just paste your token and get the decoded result. That's it."
+      />
+      <Header />
+      <CMDK />
+
+      <section className="container max-w-2xl mb-12">
+        <PageHeader
+          title="JWT Parser"
+          description="Fast, free, open source, ad-free tools."
+        />
+      </section>
+
+      <section className="container max-w-2xl mb-6">
+        <Card className="flex flex-col p-6 hover:shadow-none shadow-none rounded-xl">
+          <div>
+            <Label>JWT Token</Label>
+            <Textarea
+              rows={6}
+              placeholder="Paste JWT here"
+              onChange={handleChange}
+              className="mb-6"
+              value={input}
+            />
+
+            <div className="mb-4">
+              <Label>Decoded Header</Label>
+              <Textarea value={header} rows={6} readOnly className="mb-4" />
+              <Button
+                variant="outline"
+                onClick={() => handleCopyHeader(header)}
+              >
+                {headerText}
+              </Button>
+            </div>
+
+            <div className="mb-4">
+              <Label>Decoded Payload</Label>
+              <Textarea value={payload} rows={6} readOnly className="mb-4" />
+              <Button
+                variant="outline"
+                onClick={() => handleCopyPayload(payload)}
+              >
+                {payloadText}
+              </Button>
+            </div>
+
+            <div className="mb-4">
+              <Label>Signature</Label>
+              <Textarea value={signature} rows={2} readOnly className="mb-4" />
+              <Button
+                variant="outline"
+                onClick={() => handleCopySignature(signature)}
+              >
+                {signatureText}
+              </Button>
+            </div>
+          </div>
+        </Card>
+      </section>
+
+      <CallToActionGrid />
+    </main>
+  );
+}
