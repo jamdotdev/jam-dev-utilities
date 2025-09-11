3ab5e39255b0d6af10e54e059a7bc29311cd21e7
peckz <pekiseven@gmail.com>
2025-09-11 16:54:56 +0200

    feat: create speed test component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,11 +80,20 @@ const config = {
             transform: "scale(1)",
           },
         },
+        "subtle-pulse": {
+          "0%, 100%": {
+            opacity: "1",
+          },
+          "50%": {
+            opacity: ".5",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "grow-from-center": "grow-from-center 0.5s ease-out forwards",
+        "subtle-pulse": "subtle-pulse 1s ease-in-out infinite",
       },
     },
   },
